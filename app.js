3a3e7c4f3ca16b7fb7bfb2107a1be08cc56a4d9e
Thinlt <trungthin.luong@trueplus.vn>
2017-07-19 23:41:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Running hello nodejs");
+'use strict';
+
+const express = require('express')
+    , app = express();
+
+app.set('port', process.env.PORT || 8080)
+
+// // Required to trust GCP proxy for the
+// x-forwarded-by heading
+//app.set('trust proxy', true) 
+
+// Constants
+const HOST = '0.0.0.0';
+
+
+app.get('/', function(req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(app.get('port'), HOST);
+
+//console.log("Running hello nodejs");
