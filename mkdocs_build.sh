a18ae6b6bb21c77121a732cbdbdf44327e160f3e
Thinlt <trungthin.luong@trueplus.vn>
2017-07-20 23:49:27 +0700

    Update mkdocs_build.sh

--- mkdocs_build.sh
+++ mkdocs_build.sh
@@ -28,5 +28,5 @@ cp -Rf ${MYDOCS}/extensions/* ${MYAPP}/docs/
 
 # start build
 echo "MkDocs build"
-cd ${MYAPP} && sudo mkdocs build --clean
+cd ${MYAPP} && sudo mkdocs build
 cd ${MYAPP} && sudo mkdocs gh-deploy -q --force --remote-name https://${GITHUB_TOKEN}@github.com/Magestore/Docs.git
