c7bc2de015d8ba3274358310151797a0762bbdfb
Thinlt <trungthin.luong@trueplus.vn>
2017-07-20 22:10:10 +0700

    Update mkdocs_build.sh

--- mkdocs_build.sh
+++ mkdocs_build.sh
@@ -29,15 +29,3 @@ cp -Rf ${MYDOCS}/extensions/* ${MYAPP}/docs/
 # start build
 echo "MkDocs build"
 cd ${MYAPP} && mkdocs build
-sleep 10
-
-# Copy html built to github
-echo "Copy from ${MYAPP}/site/* to /tmp/Docs/docs/"
-git clone https://github.com/Magestore/Docs.git /tmp/Docs
-rsync -aAh ${MYAPP}/site/ /tmp/Docs/docs/
-#cp -Rf ${MYAPP}/site/* /tmp/Docs/docs/
-cd /tmp/Docs \
-   && git add . \
-   && git commit -m "Build documents" \
-   && git push origin master
-rm -rf /tmp/Docs
