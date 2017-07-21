722aa794eb92468d6996a31187d6f4e18222cb86
Thinlt <trungthin.luong@trueplus.vn>
2017-07-21 13:24:02 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -30,7 +30,8 @@ cp -f ./mkdocs_build.sh ./app/
 # run build script
 cd ./app/ && /bin/sh ./mkdocs_build.sh
 
-service cron restart
+#service cron restart
+sudo /etc/init.d/cron restart
 
 # run mkdocs with port
 mkdocs serve -a 0.0.0.0:${PORT} --dirtyreload
