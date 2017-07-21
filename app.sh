f8d2bde26cbc572156aceeeb12e85d5a4e0a6c80
Thinlt <trungthin.luong@trueplus.vn>
2017-07-21 11:33:16 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -7,7 +7,7 @@ export GITHUB_TOKEN=${GITHUB_TOKEN}
 git config --global credential.helper store
 git config --global user.email "mrthinlt@gmail.com"
 git config --global user.name "Mkdocs tool"
-echo 'https://thinlt:${GITHUB_TOKEN}@github.com' > /root/.git-credentials
+echo "https://thinlt:${GITHUB_TOKEN}@github.com" > /root/.git-credentials
 echo ${GITHUB_TOKEN} > ./GITHUB_TOKEN
 
 # create mkdocs app
