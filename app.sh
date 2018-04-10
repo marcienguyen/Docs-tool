41adb49e57538fc93049d3330e89955f2d6e4564
Thinlt <trungthin.luong@trueplus.vn>
2018-04-10 15:39:21 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -5,7 +5,7 @@
 
 # prepare github access token
 git config --global credential.helper store
-git config --global user.email "magestore.system@gmail.com"
+git config --global user.email "${GITHUB_USER}@gmail.com"
 git config --global user.name "Mkdocs tool"
 echo "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com" > /root/.git-credentials
 echo ${GITHUB_TOKEN} > ./GITHUB_TOKEN
